1dbfeeed3f31b4f7ff1e83f6fcdc35631443af0d
Shamrez <shamrezsyedali@gmail.com>
2023-07-27 19:03:00 -0400

    Clean up comments to aid readability

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,4 @@
-/* getComputerChoice will randomly return rock,
-   paper, or scissors */
-
+// Randomly returns rock, paper, or scissors
 function getComputerChoice() {
     if (Math.random() < (1/3)) {
         return 'rock';
@@ -10,16 +8,7 @@ function getComputerChoice() {
     }
 
 
-/* singleRound takes in a playerselection and the computer selection
-Check if the player selecton is same as computer, return draw
-Check if player has scissors and computer has rock, return LOSS
-Check if player has scissors and computer has paper, return WIN
-Check if player has paper and computer has scissors, return LOSS
-Check if player has paper and computer has rock, return WIN
-Check if player has rock and computer has scissors, return WIN
-Check if player has rock and computer has paper, return LOSS
-*/
-
+// Returns a win/loss message based on player and computer choice
 function singleRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === 'rock') {
         if (computerSelection === 'scissors') {
@@ -45,40 +34,38 @@ function singleRound(playerSelection, computerSelection) {
     else return 'Invalid move! You lose that turn!'
 }
 
-// function that checks result for a win
-
+// Checks result for a win
 function winStatus(result) {
     return (result.includes("win"));
 
 }
 
-// function that checks results for a draw, adds one round to the game
-
+// Checks results for a draw, adds one round to the game
 function drawStatus(result) {
     return (result.includes("Draw"));
 
 }
 
-// function that checks for a win, then +1 to count
-
 let wins = 0;
 
+// Checks for a win, then +1 to count
 function winCount(winStatus) {
     if (winStatus === true) {
         return ++wins
     } else return wins
 }
 
-// function that counts the wins and reports a victory or defeat
-
+// Counts the wins and reports a victory or defeat
 function victory(winCount) {
     if (wins >= 2) {
         return 'You win the round overall!'
     }else return 'You lose the round overall!'
 }
 
-// function that runs ONE game and returns result and win count
-
+/* Runs ONE game and returns result and win count.
+   Checks for player and computer making same choice.
+   If same choice, runs the round again.
+*/
 function playRound() {
     let playerSelection = prompt("Choose rock, paper, or scissors.");
     let computerSelection = getComputerChoice();
@@ -92,16 +79,14 @@ function playRound() {
     }
 }
 
-// function that lets the game run 3 times total (unless draw)
-
+// Lets the game run 3 times total (unless draw)
 function game() {
     for (let step = 0; step < 3; step++) {
         playRound();
     }
 }
 
-// function that runs a game and displays the victor or loser
-
+// Function that runs a game and displays the victor or loser
 function threeRoundGame(wins) {
     game();
     console.log(victory(wins))
